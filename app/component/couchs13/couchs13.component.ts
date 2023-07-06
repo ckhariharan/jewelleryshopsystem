7e538ed6edb8330b845e09d2e9c554ddf937d11a
C.K. Hariharan <133518144+ckhariharan@users.noreply.github.com>
2023-07-06 10:55:11 +0530

    Update couchs13.component.ts

--- app/component/couchs13/couchs13.component.ts
+++ app/component/couchs13/couchs13.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 import { Ser1Service } from '../../service/ser1.service';
 import { FormBuilder, Validators } from '@angular/forms';
 
-import {  Data, Router } from '@angular/router';
+import {   Router } from '@angular/router';
 import Swal from 'sweetalert2';
 @Component({
   selector: 'app-couchs13',
