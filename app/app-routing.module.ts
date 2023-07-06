d951e5912d032a79f67d3f93b92e566e3d13f73e
C.K. Hariharan <133518144+ckhariharan@users.noreply.github.com>
2023-07-06 10:36:03 +0530

    Update app-routing.module.ts

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { Component, NgModule } from '@angular/core';
+import {  NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CouchpracComponent } from './component/couchprac/couchprac.component';
 
