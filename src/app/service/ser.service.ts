3e7b059681e8e6c8b66dba337bd31742b2626836
C.K. Hariharan <133518144+ckhariharan@users.noreply.github.com>
2023-07-11 22:00:11 +0530

    Update ser.service.ts

--- src/app/service/ser.service.ts
+++ src/app/service/ser.service.ts
@@ -1,5 +1,4 @@
 import { Injectable, ViewChild } from '@angular/core';
-
 import { HttpClient } from '@angular/common/http';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
@@ -12,55 +11,53 @@ import Swal from 'sweetalert2';
 export class SerService {
   couchDBUrl: string = 'https://784ea086-d974-431c-aa48-7801aa7b2561-bluemix.cloudantnosqldb.appdomain.cloud'
   couchDBUsername: string = 'apikey-v2-23epskwaoah6sy6rvo29zejnn1k4kl1llrrq1ot36mry'
-   couchDBPassword: string = '69c1d2737d371d9f6b7f6009287e6ccc'
-   databaseName: string = 'jewel'
-   employees: Array<any> = [];
-   dataSource!: MatTableDataSource<any>;
+  couchDBPassword: string = '69c1d2737d371d9f6b7f6009287e6ccc'
+  databaseName: string = 'jewel'
+  employees: Array<any> = [];
+  dataSource!: MatTableDataSource<any>;
 
-   @ViewChild(MatPaginator) paginator!: MatPaginator;
-   @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
   private _ViewgComponent: any;
-   
-   
 
   constructor(public httpClient: HttpClient) { }
+
   updateDocument(requests: Array<any>) {
     this.httpClient.post(this.couchDBUrl + '/' + this.databaseName + '/_bulk_docs', { 'docs': requests }, {
-     headers: {
-     'Authorization': 'Basic ' + btoa(this.couchDBUsername + ':' + this.couchDBPassword)
-     }
-     }).subscribe((res: any) => {
-     if (res[0]['ok']) {
-      
-     Swal.fire('Buy successful','Thanks to Buying Our Products','success' )
-     }
-     
-     console.log("updateDocument", res);
-     })
-     }
-     async searchDocument(query: string) {
-     return this.httpClient.post(this.couchDBUrl + '/' + this.databaseName + '/_design/jw/_search/creditor_search',
-   { 'q': query, 'include_docs': true }, {
-   headers: {
-  'Authorization': 'Basic ' + btoa(this.couchDBUsername + ':' + this.couchDBPassword)
- }
-  }).subscribe((res: any) => {
-  console.log("searchDocument", res);
-   this.employees = res['rows'].map((row: any) => row['doc']);
-   this.dataSource = new MatTableDataSource(this.employees)
-   this.dataSource.paginator = this.paginator
-   this.dataSource.sort = this.sort
-  return res;
-  })
-   }
-   deleteDocument(id: string, rev: string) {
-         this.httpClient.delete(this.couchDBUrl + '/' + this.databaseName + '/' + id + '?rev=' + rev, {
-           headers: {
-             'Authorization': 'Basic ' + btoa(this.couchDBUsername + ':' + this.couchDBPassword)
-           }
-         }).subscribe(res => {
-           console.log("deleteDocument", res);
-         })
-       }
+      headers: {
+        'Authorization': 'Basic ' + btoa(this.couchDBUsername + ':' + this.couchDBPassword)
+      }
+    }).subscribe((res: any) => {
+      if (res[0]['ok']) {
+        Swal.fire('Buy successful', 'Thanks for Buying Our Products', 'success');
+      }
+      console.log("updateDocument", res);
+    });
+  }
 
+  async searchDocument(query: string) {
+    return this.httpClient.post(this.couchDBUrl + '/' + this.databaseName + '/_design/jw/_search/creditor_search',
+      { 'q': query, 'include_docs': true }, {
+        headers: {
+          'Authorization': 'Basic ' + btoa(this.couchDBUsername + ':' + this.couchDBPassword)
+        }
+      }).subscribe((res: any) => {
+        console.log("searchDocument", res);
+        this.employees = res['rows'].map((row: any) => row['doc']);
+        this.dataSource = new MatTableDataSource(this.employees);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+        return res;
+      });
+  }
+
+  deleteDocument(id: string, rev: string) {
+    this.httpClient.delete(this.couchDBUrl + '/' + this.databaseName + '/' + id + '?rev=' + rev, {
+      headers: {
+        'Authorization': 'Basic ' + btoa(this.couchDBUsername + ':' + this.couchDBPassword)
+      }
+    }).subscribe(res => {
+      console.log("deleteDocument", res);
+    });
+  }
 }
