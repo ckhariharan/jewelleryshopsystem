ff229d3ffda10d4d3ac971c10342139a6ea0482b
C.K. Hariharan <133518144+ckhariharan@users.noreply.github.com>
2023-07-14 10:59:21 +0530

    Update gbangle.component.ts

--- src/app/component/gbangle/gbangle.component.ts
+++ src/app/component/gbangle/gbangle.component.ts
@@ -12,43 +12,43 @@ export class GbangleComponent {
 
 
 
-  movehome(He:string) {
+  movehome(He: string) {
     this.sidenav.close();
-    const element =this.elementRef.nativeElement.querySelector('#${He})');
-    element.scrollIntoView({behavior:'smooth',block:'start'});
-  
+    const element = this.elementRef.nativeElement.querySelector('#${He})');
+    element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+
   }
-  
-  movebangles(B:string) {
+
+  movebangles(B: string) {
     this.sidenav.close();
-    const element =this.elementRef.nativeElement.querySelector('#${B})');
-    element.scrollIntoView({behavior:'smooth',block:'start'});
-  
+    const element = this.elementRef.nativeElement.querySelector('#${B})');
+    element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+
   }
-  moveneckles(N:string) {
+  moveneckles(N: string) {
     this.sidenav.close();
-    const element =this.elementRef.nativeElement.querySelector('#${N})');
-    element.scrollIntoView({behavior:'smooth',block:'start'});
-  
+    const element = this.elementRef.nativeElement.querySelector('#${N})');
+    element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+
   }
-  moveearrings(E:string) {
+  moveearrings(E: string) {
     this.sidenav.close();
-    const element =this.elementRef.nativeElement.querySelector('#${E})');
-    element.scrollIntoView({behavior:'smooth',block:'start'});
-  
+    const element = this.elementRef.nativeElement.querySelector('#${E})');
+    element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+
   }
-  moverings(R:string) {
+  moverings(R: string) {
     this.sidenav.close();
-    const element =this.elementRef.nativeElement.querySelector('#${R})');
-    element.scrollIntoView({behavior:'smooth',block:'start'});
-  
+    const element = this.elementRef.nativeElement.querySelector('#${R})');
+    element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+
   }
-  moveabout(A:string) {
+  moveabout(A: string) {
     this.sidenav.close();
-    const element =this.elementRef.nativeElement.querySelector('#${A})');
-    element.scrollIntoView({behavior:'smooth',block:'start'});
-  
+    const element = this.elementRef.nativeElement.querySelector('#${A})');
+    element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+
   }
-  
+
 
 }
